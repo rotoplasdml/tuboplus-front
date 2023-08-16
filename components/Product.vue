c8ef6e36e51e617cdefd332067007101952e090a
JordanRMZ <chuycrosk@gmail.com>
2023-08-15 18:02:05 -0600

    [ADD] Primer avance de componente de producto

---
+++ components/Product.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card gx-0">
+        <img src="/imagenes/soluciones-residenciales/soluciones-residenciales-abrazadera.jpg" class="card-img-top" alt="">
+        <div class="card-body bg-black">
+            <h5 class=" text-center">Tuboplus Hidraulico PP-R</h5>
+            <h5 class=" text-center">Suministro baja presion</h5>
+        </div>
+    </div>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style scoped>
+    img {
+        width: 100%;
+    }
+</style>
---
+++ pages/test/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="p-3 space" >
+    <div class="container-fluid my-3">
+        <div class="row justify-content-center gx-5">
+            <Product class="col-3" />
+            <Product class="col-3" />
+            <Product class="col-3" />
+            <Product class="col-3" />
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+import Product from '@/components/Product.vue'
+	export default {}
+</script>
+
+<style>
+.caja {
+    border: solid orange 3px;
+    background-color: aqua;
+}
+
+.space {
+    margin-top: 300px;
+}
+
+</style>
